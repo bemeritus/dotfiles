ee0cac926559e2c6e309001ac9a34edd4d7fc3f8
bemeritus <bemerituss@gmail.com>
2025-07-02 14:04:52 +0500

    feat: add docker

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,8 @@
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ]; 
 
+  virtualisation.docker.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [

1ddff428386bb813b791361df32abd6d26578d14
bemeritus <bemerituss@gmail.com>
2025-07-29 15:34:32 +0500

    chore: fix

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,15 +1,15 @@
 {...}: {
   programs.zsh = {
-    enable = true;
-    enableCompletions = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
+      enable = true;
+      # enableCompletions = true;
+      # autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
   
-    shellAliases = {
-      ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
-    };
-    history.size = 10000;
+      shellAliases = {
+        ll = "ls -l";
+        update = "sudo nixos-rebuild switch";
+      };
+      history.size = 10000;
   };
 }
 

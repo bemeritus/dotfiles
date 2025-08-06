d5fd5b7d9621beb266c2e95eaa13c93bf451a4f3
bemeritus <bemerituss@gmail.com>
2025-08-06 16:09:05 +0500

    fix(zed.nix): rewrite config

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -1,9 +1,160 @@
-{...}: {
-  programs.zed-editor = {
-    enable = true;
-    extensions = [
-      "nix"
-      "toml"
-    ];
+{pkgs, ...}: let
+  extensions = [
+    "assembly"
+    "deno"
+    "env"
+    "glsl"
+    "haskell"
+    "html"
+    "ini"
+    "just"
+    "latex"
+    "lua"
+    "make"
+    "material-icon-theme"
+    "neocmake"
+    "nginx"
+    "nix"
+    "nu"
+    "pkl"
+    "ruby"
+    "slint"
+    "sql"
+    "swift"
+    "toml"
+    "typst"
+    "vercel-theme"
+    "wgsl"
+    "xml"
+    "zig"
+  ];
+
+  settings = {
+    auto_update = false;
+
+    disable_ai = true;
+
+    telemetry = {
+      metrics = false;
+      diagnostics = false;
+    };
+
+    show_edit_predictions = false;
+
+    languages = {
+      Markdown = {
+        format_on_save = "on";
+        use_on_type_format = true;
+        remove_trailing_whitespace_on_save = true;
+      };
+
+      Nix = {
+        formatter = "language_server";
+        language_servers = [
+          "nixd"
+          "!nil"
+        ];
+      };
+    };
+
+    lsp = {
+      nixd = {
+        binary = {
+          ignore_system_version = false;
+        };
+        settings = {
+          formatting = {
+            command = [
+              "alejandra"
+            ];
+          };
+          diagnostic = {
+            suppress = [
+              "sema-extra-with"
+              "sema-extra-rec"
+            ];
+          };
+        };
+      };
+
+      rust-analyzer = {
+        binary = {
+          ignore_system_version = false;
+        };
+        initialization_options = {
+          check = {
+            command = "clippy";
+          };
+        };
+      };
+
+      solargraph = {
+        binary = {
+          ignore_system_version = false;
+        };
+        initialization_options = {
+          diagnostics = true;
+          formatting = true;
+        };
+      };
+    };
+
+    load_direnv = "shell_hook";
+
+    theme = {
+      mode = "system";
+      light = "Gruvbox Light";
+      dark = "Gruvbox Dark";
+    };
+    icon_theme = "Material Icon Theme";
+
+    tab_size = 2;
+    preferred_line_length = 100;
+
+    autosave = "true";
+    format_on_save = "language_server";
+    enable_language_server = true;
+
+    soft_wrap = "editor_width";
+
+    buffer_font_size = 20;
+    buffer_font_family = "mononoki";
+
+    ui_font_size = 20;
+    ui_font_family = ".SystemUIFont";
+
+    confirm_quit = false;
+    use_autoclose = false;
+
+    inlay_hints = {
+      enabled = true;
+      # show_background = true;
+    };
+
+    title_bar = {
+      show_branch_icon = true;
+    };
+
+    collaboration_panel = {
+      button = false;
+    };
+
+    chat_panel = {
+      button = "never";
+    };
+
+    agent = {
+      enabled = false;
+    };
+  };
+in {
+  config = {
+    programs.zed-editor = {
+      enable = true;
+      inherit extensions;
+      userSettings = settings;
+      installRemoteServer = true;
+      extraPackages = with pkgs; [nixd];
+    };
   };
 }
